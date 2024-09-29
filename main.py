a74cf9dfac1b5073c29b2fe9932e80fe5bfb19b0
Marco Taylor <146408980+ACCSCI@users.noreply.github.com>
2024-09-29 13:15:58 +0800

    Local_Win11_Running_Configuration

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from logging import DEBUG
-import gevent
+import logging
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
 
@@ -48,11 +47,24 @@ def sendMsg(msg):
         "user":request.sid,
     },to=msg['room'])
 
+class HttpsErrSuppressFilter(logging.Filter):
+    def filter(self, record):
+        msg = record.msg
+        if msg.find("Invalid http version") != -1 or \
+           msg.find("Expected GET method") != -1 or \
+           msg.find("Invalid HTTP method") != -1 or \
+           msg.find("400 -"):
+            return False
+        return True
+
 if __name__=="__main__":
     # app.run(debug=True,threaded=True)
     # socketio.run(app)
+    logger = logging.getLogger("__name__")
+    logger.addFilter(HttpsErrSuppressFilter())
+    logger.propagate = False
     # 打印启动信息
-    print(f" * Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)")
+    print(f" * Running on http://127.0.0.1:8000/ (Press CTRL+C to quit)")
     # 使用 gevent 的 WSGIServer 启动应用
-    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler)
+    server = pywsgi.WSGIServer(('127.0.0.1', 8000), app, handler_class=WebSocketHandler,error_log=logger)
     server.serve_forever()
